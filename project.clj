8ac2d343b647c55acfde82cc0a6909959a30ef5f
jmatt <jmatt@jmatt.org>
2011-09-08 18:21:00 -0700

    Adhoc Webserver for Local Development. Go!

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lessc "0.1.1"
+  :description "Strap a Webserver Together."
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [compojure "0.6.4"]
+                 [hiccup "0.3.6"]]
+  :dev-dependencies [[lein-ring "0.4.5"]]
+  :ring {:handler jmatt.strap.routes/app})
---
+++ src/jmatt/strap/views.clj
@@ -0,0 +1,14 @@
+(ns jmatt.strap.views
+  (:use [hiccup core page-helpers]))
+
+(defn index-page []
+  (html5
+    [:head
+      [:title "Strap"]
+     (include-css "/css/reset-min.css")
+     (include-css "/css/base-min.css")
+     (include-css "/css/style.css")]
+    [:body
+     [:div {:class "container"}
+     [:h1 "Strap"]
+      [:h3 "Just put your website in <code>resources/public</code> and type <code>lein ring server</code>."]]]))
