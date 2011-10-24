c935f301dd47de2957c56c60025a23bcd9d17e0e
J. Matt Peterson <jmatt@jmatt.org>
2011-10-24 13:33:48 -0700

    Updated version and some layout for clojar push of v0.1.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject strap "0.1.1"
+(defproject org.jmatt/strap "0.1.2"
   :description "Strap a Webserver Together."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [compojure "0.6.4"]
                  [hiccup "0.3.6"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
-  :ring {:handler jmatt.strap.routes/app})
+  :ring {:handler strap.routes/app})
